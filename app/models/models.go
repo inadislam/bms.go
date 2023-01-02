b6564a5a5e7863ef2cf91684b35e70ddb3dcffd1
inadislam <inadislam@gmail.com>
2023-01-02 18:16:09 +0600

    ✅ models added

--- app/models/models.go
+++ app/models/models.go
@@ -1,1 +1,24 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Users struct {
+	ID           uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key;" json:"id"`
+	Name         string    `gorm:"type:varchar(255);not null" json:"name"`
+	Email        string    `gorm:"uniqueIndex;not null" json:"email"`
+	Password     string    `gorm:"not null" json:"password"`
+	Role         string    `gorm:"type:varchar(255);not null" json:"role"`
+	ProfilePhoto string    `gorm:"type:varchar(255);not null" json:"profile_photo"`
+	Verification string    `gorm:"type:varchar(255);uniqueIndex;not null" json:"verification"`
+	Verified     bool      `gorm:"not null" json:"verified"`
+	CreatedAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+type Login struct {
+	Email    string `gorm:"uniqueIndex;not null" json:"email"`
+	Password string `gorm:"not null" json:"password"`
+}
